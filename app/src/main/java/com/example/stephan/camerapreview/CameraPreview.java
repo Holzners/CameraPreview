d63a0781c45d663950f9d3edb806b31f2643c04d
holzners <von-mir1@gmx.de>
2015-06-09 13:02:53 +0200

    CameraPreview überarbeitet

--- app/src/main/java/com/example/stephan/camerapreview/CameraPreview.java
+++ app/src/main/java/com/example/stephan/camerapreview/CameraPreview.java
@@ -1,61 +1,98 @@
 package com.example.stephan.camerapreview;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.Gravity;
+import android.view.TextureView;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
 
-public class CameraPreview extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-    private SurfaceView preview=null;
-    private SurfaceHolder previewHolder=null;
+
+public class CameraPreview extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+                                                    TextureView.SurfaceTextureListener{
+
     private Camera camera=null;
-    private boolean inPreview=false;
-    private boolean cameraConfigured=false;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
+    private TextureView mTextureView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera_preview);
 
-        preview=(SurfaceView)findViewById(R.id.preview);
-        previewHolder=preview.getHolder();
-        previewHolder.addCallback(surfaceCallback);
-        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.contentPanel);
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+
+        frameLayout.addView(mTextureView);
+        Button button = (Button) findViewById(R.id.navigationButton);
+        button.setBackground(this.getResources().getDrawable(R.drawable.navigation_icon));
+        frameLayout.removeView(button);
+        frameLayout.addView(button);
+
         buildGoogleApiClient();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    public void newNavigation(View view){
 
-        camera=Camera.open();
-        startPreview();
+    }
+
+    public Location getLocation(){
+        return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
-    public void onPause() {
-        if (inPreview) {
-            camera.stopPreview();
+    public void onConnected(Bundle bundle) {
+        mLastLocation = getLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        camera = Camera.open();
+
+        Camera.Parameters parameters=camera.getParameters();
+        Camera.Size previewSize = getBestPreviewSize(width, height,
+                parameters);
+
+        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
+                previewSize.width, previewSize.height, Gravity.CENTER));
+
+        try {
+            camera.setPreviewTexture(surface);
+        } catch (IOException t) {
+            t.printStackTrace();
         }
 
-        camera.release();
-        camera=null;
-        inPreview=false;
+        camera.startPreview();
 
-        super.onPause();
     }
 
     private Camera.Size getBestPreviewSize(int width, int height,
@@ -81,84 +118,20 @@ public class CameraPreview extends Activity implements GoogleApiClient.Connectio
         return(result);
     }
 
-    private void initPreview(int width, int height) {
-        if (camera!=null && previewHolder.getSurface()!=null) {
-            try {
-                camera.setPreviewDisplay(previewHolder);
-            }
-            catch (Throwable t) {
-                Log.e("CameraPreview",
-                        "Exception in setPreviewDisplay()", t);
-                Toast
-                        .makeText(CameraPreview.this, t.getMessage(), Toast.LENGTH_LONG)
-                        .show();
-            }
-
-            if (!cameraConfigured) {
-                Camera.Parameters parameters=camera.getParameters();
-                Camera.Size size=getBestPreviewSize(width, height,
-                        parameters);
-
-                if (size!=null) {
-                    parameters.setPreviewSize(size.width, size.height);
-                    camera.setParameters(parameters);
-                    cameraConfigured=true;
-                }
-            }
-        }
-    }
-
-    private void startPreview() {
-        if (cameraConfigured && camera!=null) {
-            camera.startPreview();
-            inPreview=true;
-        }
-    }
-
-    SurfaceHolder.Callback surfaceCallback  =new SurfaceHolder.Callback() {
-        public void surfaceCreated(SurfaceHolder holder) {
-            // no-op -- wait until surfaceChanged()
-        }
-
-        public void surfaceChanged(SurfaceHolder holder,
-                                   int format, int width,
-                                   int height) {
-            initPreview(width, height);
-            startPreview();
-        }
-
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // no-op
-        }
-    };
-
-    public void newNavigation(View view){
-
-    }
-
-    public Location getLocation(){
-        return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-    }
-    private synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
     @Override
-    public void onConnected(Bundle bundle) {
-        mLastLocation = getLocation();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
 
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        camera.stopPreview();
+        camera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
 
     }
 }
